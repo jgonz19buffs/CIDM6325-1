cb5661a13eff00df68b67ca208379cbe41339b94
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-09-30 13:48:03 -0500

    Preventing users from using an existing email address, correction

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -28,7 +28,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     def clean_email(self):
         data = self.cleaned_data['email']
-        if User.objects.filter(email=data).exists():
+        if get_user_model().objects.filter(email=data).exists():
             raise forms.ValidationError('Email already in use.')
         return data
 
@@ -37,16 +37,16 @@ class UserEditForm(forms.ModelForm):
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
 
-        def clean_email(self):
-            data = self.cleaned_data['email']
-            qs = User.objects.exclude(
-                id=self.instance.id
-            ).filter(
-                email=data
-            )
-            if qs.exists():
-                raise forms.ValidationError('Email already in use.')
-            return data
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = get_user_model().objects.exclude(
+            id=self.instance.id
+        ).filter(
+            email=data
+        )
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
