e614e2f7edce90ab9e33725a4d9d2c69ae8eb2f7
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-01 17:03:51 -0500

    Adding infinite scroll pagination to the image list

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm, Image
@@ -55,3 +56,33 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page of range
+            # return an empty page
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
