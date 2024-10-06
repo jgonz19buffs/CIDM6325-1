d3e6f30358dea24567c252c9413d60b1059e7670
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-06 11:01:34 -0500

    installing django debug toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -57,9 +57,11 @@ INSTALLED_APPS = [
     'django_extensions',
     # thumbnails
     'easy_thumbnails',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -164,3 +166,7 @@ SOCIAL_AUTH_PIPELINE = [
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
